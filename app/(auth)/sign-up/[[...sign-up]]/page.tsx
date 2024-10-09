5938001a183eb73fbaec3dfe50ba91825f3a2cea
Ashley <99146042+Ashley0324@users.noreply.github.com>
2024-10-09 14:55:56 +0800

    Update page.tsx
    sign-up modify

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,27 @@
 import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignUp />;
+  return(
+    <div className="h-full flex flex-row">
+      <div className="basis-1/2 bg-[#222222] flex items-center text-green-400 justify-center flex-col">
+        <div className="text-heading1-bold">
+          ✨
+        </div>
+        <div className="text-body-normal text-white">
+          From DAOs to Network State
+        </div>
+        <div className="text-heading1-bold">
+          GREENBOOK:
+        </div>
+        <div className="text-heading1-bold">
+          Free, Connect, Flow
+        </div>
+      </div>
+      <div className="basis-1/2 h-full w-full bg-[#80FC03] flex items-center justify-center">
+        <SignUp />
+      </div>
+    </div>
+  );
 }
+
